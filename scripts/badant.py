9be4277a8e1bfea4cd23c55a62e2a9861729de24
Casey Law <caseyjlaw@gmail.com>
2022-12-08 21:49:05 +0000

    notes

--- scripts/badant.py
+++ scripts/badant.py
@@ -3,12 +3,13 @@ from lwa_antpos import mapping
 
 con = control.Controller("/home/ubuntu/proj/lwa-shell/mnc_python/config/lwa_config_calim.yaml")
 
-# find bad antennas from threshodl on arx channel power
+# find bad antennas from threshold on arx channel power
 adrs = con.conf['arx']['adrs']
 ma = lwa_arx.ARX()
 adr = adrs[0]
 chpow = ma.get_all_chan_power(adr)
-# threshold then map chpow to antenna
+# threshold on power
+# map chpow to antenna
 
 snap2nums = [name.lstrip('snap') for name in con.conf['fengines']['snap2s_inuse']]
 num = snap2nums[0]
@@ -18,7 +19,7 @@ print(mp['time'], mp['stats']['pfb']['overflow_count'], mp['stats']['fpga']['pro
 # threshold then map to bad antenna groups on a snap
 
 statsdict = mp['stats']['input']
-#threshold on 'powernn', 'meannn', 'rmsnn'
+# threshold on 'powernn', 'meannn', 'rmsnn'
 # map to bad antenna
 
 # reserve /mon/antvisflag/n for flags identified from visibilities
