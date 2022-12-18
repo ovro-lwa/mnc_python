f226fa38b753b4afc9331e81d08a5e301fc24475
Casey Law <caseyjlaw@gmail.com>
2022-12-18 18:13:57 +0000

    removing myst requirement

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ author = 'Casey Law'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", 'sphinx.ext.autodoc','sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc','sphinx.ext.viewcode']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
