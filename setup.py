60f00548f2662a3aa02b2e2c302cc9b45e138e59
Casey Law <claw@lwacalim00.mcs.pvt>
2023-03-13 20:24:34 +0000

    change to versioning for pep 440 compat

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 from version import get_git_version
+from setuptools_scm import get_version
 
 setup(name='mnc-python',
-      version=get_git_version(),
+      version=get_version(),
       url='http://github.com/ovro-lwa/mnc-python',
-      requirements=['astropy', 'progressbar', 'myst-parser'],
+      requirements=['astropy', 'progressbar', 'myst-parser', 'setuptools_scm'],
       packages=['mnc'],
       zip_safe=False)
