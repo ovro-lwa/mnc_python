7be2c722c169978cfe1bbdcf1aa44cdb5bbbf0f0
Casey Law <caseyjlaw@gmail.com>
2023-07-27 01:32:18 +0000

    toy influx query function

---
+++ mnc/influx.py
@@ -0,0 +1,15 @@
+from influxdb import DataFrameClient
+from astropy.time import Time
+influx = DataFrameClient('influxdbservice.sas.pvt', 8086, 'root', 'root', 'dsa110')
+
+MS_PER_SECOND = 1000
+
+def get_rfpower(utctime, dt):
+    """ Given utctime (e.g., '2023-07-25T10:00:00') and dt in seconds, return ARX RF power monitor points.
+    """
+    
+    tu = int(MS_PER_SECOND*Time(utctime, format='isot').unix)
+    query = f'SELECT time, ant_num, "power_a" as pa, "power_b" as pb FROM "antmon" WHERE time >= {tu}ms and time < {tu+dt*MS_PER_SECOND}ms'
+    result = influx.query(query)['antmon']
+
+    return result
