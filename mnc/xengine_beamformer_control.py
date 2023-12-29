97e7dc9782d25c8f6d28dc7bd559b455d84e2dc0
Casey Law <caseyjlaw@gmail.com>
2023-12-29 00:56:42 +0000

    Fix gain calculation bug in BeamPointingControl

--- mnc/xengine_beamformer_control.py
+++ mnc/xengine_beamformer_control.py
@@ -300,8 +300,8 @@ class BeamPointingControl(object):
                 for pol in range(NPOL):
                     cal = 1./caldata[j,i*NCHAN_PIPELINE:(i+1)*NCHAN_PIPELINE,pol].ravel()
                     cal = numpy.where(numpy.isfinite(cal), cal, 0)
-#                    flg = flgdata[j,i*NCHAN_PIPELINE:(i+1)*NCHAN_PIPELINE,pol].ravel()
-#                    cal *= (1-flg)
+                    flg = flgdata[j,i*NCHAN_PIPELINE:(i+1)*NCHAN_PIPELINE,pol].ravel()
+                    cal *= (1-flg)
                     to_execute.append(push_gains(p, ii, 2*(self.beam-1)+pol, NPOL*j+pol, cal))
         loop.run_until_complete(asyncio.gather(*to_execute, loop=loop))
         loop.close()
