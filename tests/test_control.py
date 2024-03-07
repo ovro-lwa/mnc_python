46af8e12a769235f03dbdd8322b8660217e78bf0
Casey Law <caseyjlaw@gmail.com>
2024-03-07 22:15:49 +0000

    clean up tests

--- tests/test_control.py
+++ tests/test_control.py
@@ -6,20 +6,9 @@ class TestController(unittest.TestCase):
     def setUp(self):
         self.controller = Controller()
 
-    def test_init_no_args(self):
-        with self.assertRaises(TypeError):
-            Controller()
-
     def test_init_with_args(self):
-        controller = Controller('arg1', 'arg2')
-        self.assertEqual(controller.arg1, 'arg1')
-        self.assertEqual(controller.arg2, 'arg2')
-
-    @patch('control.Controller.dsa_store')
-    def test_mock_dsa_store(self, mock_dsa_store):
-        mock_dsa_store.return_value = 'mocked'
-        result = self.controller.dsa_store('arg1', 'arg2')
-        self.assertEqual(result, 'mocked')
+        controller = Controller(recorders='dr5')
+        self.assertEqual(controller.conf['dr']['recorders'], ['dr5'])
 
 if __name__ == '__main__':
     unittest.main()
