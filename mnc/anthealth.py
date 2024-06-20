2b34dd7a82e47a8f12ea84d4e6b8b58057bf2214
Casey Law <caseyjlaw@gmail.com>
2024-06-20 20:56:15 +0000

    feat: Remove blank entries from the list of past badant measurements
    This commit removes blank entries from the list of past badant measurements in the `get_badants` function. It ensures that only valid entries are considered for further processing, improving the accuracy of the badant status tracking feature.

--- mnc/anthealth.py
+++ mnc/anthealth.py
@@ -91,9 +91,9 @@ def get_badants(method, time=None, naming='ant'):
     # get times of past badant lists
     et = ls.get_etcd()
     mjds = sorted([kv.key.decode().lstrip('/mon/anthealth/selfcorr/') for _, kv in et.get_prefix('/mon/anthealth/selfcorr')])
-    mjds = filter(lambda x: x != '', mjds)  # remove blank entries
+    mjds = list(filter(lambda x: x != '', mjds))  # remove blank entries
     if len(mjds) > 0:
-        mjd0 = filter(lambda x: float(x) < mjd, mjds)[-1]  # get the latest before mjd
+        mjd0 = list(filter(lambda x: float(x) < mjd, mjds))[-1]  # get the latest before mjd
     else:
         logger.error("No badant found with mjds. Using default.")
         mjd0 = None
