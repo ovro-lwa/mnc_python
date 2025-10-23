b53c6dfedb0261f4abdb35a80c486fcea1c01ef4
Casey Law <caseyjlaw@gmail.com>
2025-10-23 23:40:02 +0000

    fixing -s flag and setting to 10 (100s extraction interval)

--- scripts/extract_lastnight.sh
+++ scripts/extract_lastnight.sh
@@ -12,7 +12,7 @@ msdate=$last
 hrs=(06 07 08 09 10 11 12)
 for hr in ${hrs[@]}
 do 
-    python3 $scriptdir/extract_autocor.py -p /lustre/pipeline/slow/ -d 30 -d $msdate -t $hr -w $workingdir
+    python3 $scriptdir/extract_autocor.py -p /lustre/pipeline/slow/ -s 10 -d $msdate -t $hr -w $workingdir
 done
 
 workingdir=$workingdir$last"/"
