35c7075088627cc7137a610b799850e3d5b58312
Casey Law <caseyjlaw@gmail.com>
2026-02-12 00:23:28 +0000

    Add pyproject.toml for build system configuration and update setup.py dependencies

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(name='mnc-python',
       install_requires=['astropy',
                         'progressbar',
                         'myst-parser',
-                        'setuptools_scm<7',
                         'markdown<3.4'],
       packages=['mnc'],
       entry_points='''
